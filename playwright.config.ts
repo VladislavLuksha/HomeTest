15afa8eb450743e7178ade6486370b1aa8a315f1
VladislavLuksha <vladislavluksha99@gmail.com>
2025-06-03 13:41:22 +0300

    Updated configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './test/specs/ui',
+  timeout: 30000,
+  expect: {
+    timeout: 5000,
+  },
   use: {
     baseURL: 'https://www.saucedemo.com',
+    trace: 'on-first-retry',
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
-  reporter: [['list'], ['allure-playwright']],
-  timeout: 30000,
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ],
+  reporter: [['list'], ['allure-playwright'], ['html']],
+  outputDir: 'test-results/'
 });
